03f9d3ab32834988028f51a1fa41ab8f3c06c232
Jeremy Friedland <jeremyfriedland@gmail.com>
2016-05-25 19:25:23 -0700

    Initial Commit

---
+++ Assignment 2: CarouselTests/Assignment_2__CarouselTests.swift
@@ -0,0 +1,36 @@
+//
+//  Assignment_2__CarouselTests.swift
+//  Assignment 2: CarouselTests
+//
+//  Created by Jeremy Friedland on 5/25/16.
+//  Copyright © 2016 Jeremy Friedland. All rights reserved.
+//
+
+import XCTest
+@testable import Assignment_2__Carousel
+
+class Assignment_2__CarouselTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
